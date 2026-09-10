db77e8375107b2f17e25ebc0e698045527b80efc
weili <weili.walk@gmail.com>
2026-09-10 00:39:33 -0700

    privacy: one page for every version of Beagle

--- beagle-web.js
+++ beagle-web.js
@@ -1,4 +1,4 @@
-globalThis.__BEAGLE_BUILD__={"peer":"0.1.164","ui":"0.2.7","builtAt":"2026-09-10T07:26:00.720Z"};
+globalThis.__BEAGLE_BUILD__={"peer":"0.1.164","ui":"0.2.7","builtAt":"2026-09-10T07:39:28.902Z"};
 (() => {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
