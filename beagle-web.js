0a488fb74c0a209965fc7ac9a0fda4d789833c20
weili <weili.walk@gmail.com>
2026-09-09 22:41:37 -0700

    privacy page: self-contained, and it fits a phone

--- beagle-web.js
+++ beagle-web.js
@@ -1,4 +1,4 @@
-globalThis.__BEAGLE_BUILD__={"peer":"0.1.164","ui":"0.2.7","builtAt":"2026-09-09T05:31:09.409Z"};
+globalThis.__BEAGLE_BUILD__={"peer":"0.1.164","ui":"0.2.7","builtAt":"2026-09-10T05:41:32.159Z"};
 (() => {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
