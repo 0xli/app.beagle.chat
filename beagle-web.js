4df151e5d6e611bc004e0dc5e3b1c54cdbb7f6a9
weili <weili.walk@gmail.com>
2026-09-12 21:21:00 -0700

    sync from beagle-web

--- beagle-web.js
+++ beagle-web.js
@@ -1,4 +1,4 @@
-globalThis.__BEAGLE_BUILD__={"peer":"0.1.164","ui":"0.2.8","builtAt":"2026-09-13T02:46:26.834Z"};
+globalThis.__BEAGLE_BUILD__={"peer":"0.1.164","ui":"0.2.8","builtAt":"2026-09-13T04:20:55.735Z"};
 (() => {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
